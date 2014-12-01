74a6748918801e1b3a6f87fe884367f5355f9239
KaiHofstetter <kai.hofstetter@gmx.de>
2014-12-01 20:41:43 +0100

    OAuth2RestTemplate client for the Authorization Code uses an injected OAuth2ClientContext.

--- src/main/java/net/softwareminds/foosballbooking/client/config/OAuth2RestTemplatesConfig.java
+++ src/main/java/net/softwareminds/foosballbooking/client/config/OAuth2RestTemplatesConfig.java
@@ -1,21 +1,16 @@
 package net.softwareminds.foosballbooking.client.config;
 
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
-import org.springframework.context.annotation.ScopedProxyMode;
-import org.springframework.security.oauth2.client.DefaultOAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2RestOperations;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
-import org.springframework.security.oauth2.client.token.AccessTokenRequest;
 import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsAccessTokenProvider;
 import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsResourceDetails;
 import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeAccessTokenProvider;
 import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
-
-import javax.annotation.Resource;
 
 @Configuration
 @EnableOAuth2Client
@@ -24,12 +19,10 @@ public class OAuth2RestTemplatesConfig {
   private static final String ACCESS_TOKEN_URI = "http://localhost:8080/foosball-booking-service/oauth/token";
   private static final String USER_AUTHORIZATION_URI = "http://localhost:8080/foosball-booking-service/oauth/authorize";
 
-  @Resource
-  @Qualifier("accessTokenRequest")
-  private AccessTokenRequest accessTokenRequest;
+  @Autowired
+  private OAuth2ClientContext oauth2ClientContext;
 
   @Bean
-  @Scope(value = "session", proxyMode = ScopedProxyMode.INTERFACES)
   public OAuth2RestOperations bookingAuthorizationCodeClient() {
     AuthorizationCodeResourceDetails resourceDetails = new AuthorizationCodeResourceDetails();
     resourceDetails.setId("1");
@@ -38,7 +31,7 @@ public class OAuth2RestTemplatesConfig {
     resourceDetails.setAccessTokenUri(ACCESS_TOKEN_URI);
     resourceDetails.setUserAuthorizationUri(USER_AUTHORIZATION_URI);
 
-    OAuth2RestTemplate oAuth2RestTemplate = new OAuth2RestTemplate(resourceDetails, new DefaultOAuth2ClientContext(accessTokenRequest));
+    OAuth2RestTemplate oAuth2RestTemplate = new OAuth2RestTemplate(resourceDetails, oauth2ClientContext);
     oAuth2RestTemplate.setAccessTokenProvider(new AuthorizationCodeAccessTokenProvider());
     return oAuth2RestTemplate;
   }
